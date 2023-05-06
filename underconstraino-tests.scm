17144788d067ee30fecefc95d5bdd530407b14ef
William Byrd <webyrd@gmail.com>
2023-05-06 07:50:47 -0400

    added tests

--- underconstraino-tests.scm
+++ underconstraino-tests.scm
@@ -15,21 +15,21 @@
 ;; implementation, and to make it easy to think about basic properties
 ;; of soundness/unsoundess and goal ordering.
 
-(test "underconstraino-1a"
+(test "underconstraino-trivial-classic"
   ;; classic miniKanren
   (run* (x)
     (== 3 x)
     (== 4 x))
   '())
 
-(test "underconstraino-1b"
+(test "underconstraino-trivial-unsound"
   ;; unsound!
   (run* (x)
     (underconstraino (== 3 x))
     (underconstraino (== 4 x)))
   '(_.0))
 
-(test "underconstraino-1c"
+(test "underconstraino-trivial-sound-but-useless"
   ;; sound but with a useless ordering of goals
   (run* (x)
     (== 3 x)
@@ -38,7 +38,7 @@
     (underconstraino (== 4 x)))
   '(_.0))
 
-(test "underconstraino-1d"
+(test "underconstraino-trivial-sound-but-useless-2"
   ;; also sound but with a useless ordering of goals
   (run* (x)
     (underconstraino (== 3 x))
@@ -47,7 +47,7 @@
     (underconstraino (== 4 x)))
   '(_.0))
 
-(test "underconstraino-1e"
+(test "underconstraino-trivial-typical"
   ;; sound, with a typical goal ordering for underconstraints:
   ;; underconstraints should normally come first
   (run* (x)
@@ -57,7 +57,7 @@
     (== 4 x))
   '(_.0))
 
-(test "underconstraino-1f"
+(test "underconstraino-trivial-optimized"
   ;; sound, with a typical goal ordering for underconstraints, and
   ;; with the underconstraint for the first non-constrained goal
   ;; removed, since it is not helpful with this particular goal
@@ -72,3 +72,65 @@
     (== 4 x))
   '(_.0))
 
+
+;; Now for a slightly more realistic example.
+
+(define one-or-two-choiceo
+  (lambda (x)
+    (conde
+      ((== 1 x))
+      ((== 2 x)))))
+
+(define three-or-four-choiceo
+  (lambda (x)
+    (conde
+      ((== 3 x))
+      ((== 4 x)))))
+
+;; classic miniKanren
+(test "underconstraino-number-choiceo-classic"
+  (run* (x)
+    (one-or-two-choiceo x)
+    (three-or-four-choiceo x))
+  '())
+
+;; unsound!
+(test "underconstraino-number-choiceo-unsound"
+  (run* (x)
+    (underconstraino (one-or-two-choiceo x))
+    (underconstraino (three-or-four-choiceo x)))
+  '(_.0))
+
+;; sound but with a useless ordering of goals
+(test "underconstraino-number-choiceo-sound-but-useless"
+  (run* (x)
+    (one-or-two-choiceo x)
+    (three-or-four-choiceo x)
+    (underconstraino (one-or-two-choiceo x))
+    (underconstraino (three-or-four-choiceo x)))
+  '())
+
+;; sound, with a typical goal ordering for underconstraints:
+;; underconstraints should normally come first
+(test "underconstraino-number-choiceo-typical"
+  (run* (x)
+    (underconstraino (one-or-two-choiceo x))
+    (underconstraino (three-or-four-choiceo x))
+    (one-or-two-choiceo x)
+    (three-or-four-choiceo x))
+  '())
+
+;; sound, with a typical goal ordering for underconstraints, and
+;; with the underconstraint for the first non-constrained goal
+;; removed, since it is not helpful with this particular goal
+;; ordering
+;;
+;; it may be desirable to keep both underconstraints, if the goals
+;; might be reordered in the future, or if the goals might be
+;; dynamically reordered at runtime
+(test "underconstraino-number-choiceo-optimized"
+  (run* (x)
+    (underconstraino (three-or-four-choiceo x))
+    (one-or-two-choiceo x)
+    (three-or-four-choiceo x))
+  '())
