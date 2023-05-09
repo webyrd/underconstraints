41b1b93c2f0137e1a1dfcde26227df11b1ebdb11
William Byrd <webyrd@gmail.com>
2023-05-09 13:02:02 -0400

    added commented parameter setting expressions to test-under.scm for convenience

--- faster-minikanren/test-under.scm
+++ faster-minikanren/test-under.scm
@@ -3,5 +3,12 @@
 (load "numbers.scm")
 (load "test-check.scm")
 
+#|
+;; can set global underconstraint parameter values:
+
+  (*underconstraint-default-timeout-param* #f)
+  (*trace-underconstraint-param* #f)
+|#
+
 (load "test-underconstraints-one-shot.scm")
 ;; (load "test-underconstraints-general.scm")
